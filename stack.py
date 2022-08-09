59552a1a7614be087c77c736902ae18d633a022c
honeybeeveloper <honeybeeveloper@naver.com>
2022-08-09 23:36:40 +0900

    Stack

---
+++ stack.py
@@ -0,0 +1,60 @@
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        if self.stack:
+            return False
+        return True
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        deleted = None
+        if self.is_empty():
+            print('Stack is empty!')
+        else:
+            deleted = self.stack.pop()
+        return deleted
+
+
+class Node(object):
+    def __init__(self, value, next_pointer=None):
+        self.value = value
+        self.next = next_pointer
+
+
+class StackLinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        if self.head:
+            return False
+        return True
+
+    def push(self, data):
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+
+    def pop(self):
+        deleted = None
+        if self.is_empty():
+            print('Stack is empty!')
+        else:
+            deleted = self.head.value
+            self.head = self.head.next
+        return deleted
+
+
+if __name__ == '__main__':
+    stack = StackLinkedList()
+    stack.pop()
+    stack.push('a')
+    stack.push('b')
+    stack.pop()
+    stack.push('d')
+    stack.pop()
+    stack.push('c')
